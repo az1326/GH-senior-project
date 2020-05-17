df8dd1dc2ec43f29bce8f71721f99935589b468d
Alan Zhu <alanzhu2002@gmail.com>
2020-05-16 21:23:19 -0400

    Add rules

--- SpaceGame/src/view/Rules.java
+++ SpaceGame/src/view/Rules.java
@@ -10,7 +10,11 @@ public class Rules extends JPanel{
     private JLabel rules;
 
     public Rules() {
-        rules = new JLabel("<html><div style='text-align: center;'>test1<br>test2<br>test3<div></html>");
+        rules = new JLabel("<html><div style='text-align: center;'>Welcome to SpaceGame! Defend your base from "
+        + "asteroids.<br>Use the mouse to move your ship and click to fire a laser.<br>Be economical, you can only " 
+        + "have 5 lasers active at any time.<br>Pick up powerups to repair damage or activate rapid fire." 
+        + "<br>The game is over when your base health or ship health is<br>reduced to 0. Survive as long as you can!" 
+        + "<div></html>");
         rules.setForeground(Color.WHITE);
         rules.setHorizontalAlignment(SwingConstants.CENTER);
 
